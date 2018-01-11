3ea5db977c750337123d87293cbe748356c0a629
Paul Cutler <paul.r.cutler@gmail.com>
2018-01-11 08:19:15 -0600

    Add sample configuration file
    Sample configuration file for MySportsfeeds, admin and database

---
+++ data/config-example.py
@@ -0,0 +1,13 @@
+# You will need an account from MySportsFeed to access their API.  They offer free access to developers
+# Edit below with your credentials and then save as config.py
+
+msf_username = ''
+msf_pw = ''
+
+
+# Enter the email address of the user who will be the administrator / superuser
+su_email = ''
+
+# Enter your database user and password
+db_user = ''
+db_pw = ''
