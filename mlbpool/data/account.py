8bbfe4e54ea6524e4634bcca951cd2df0055ee58
Paul Cutler <paul.r.cutler@gmail.com>
2018-01-16 10:49:18 -0600

    Begin refactor to use MySQL instead of sqlite
    Database connects and creates the first table (Account).  But
    connecting to the db using Sequel Pro causes Sequel Pro to crash
    making me think I have the table types incorrect.  (But it connects!)

--- mlbpool/data/account.py
+++ mlbpool/data/account.py
@@ -7,14 +7,14 @@ from mlbpool.data.modelbase import SqlAlchemyBase
 class Account(SqlAlchemyBase):
     __tablename__ = 'Account'
 
-    id = Column(String, primary_key=True,
+    id = Column(String(32), primary_key=True,
                 default=lambda: str(uuid.uuid4()).replace('-', ''))
 
-    email = Column(String, index=True, unique=True, nullable=False)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    password_hash = Column(String)
+    email = Column(String(32), index=True, unique=True, nullable=False)
+    first_name = Column(String(16), nullable=False)
+    last_name = Column(String(32), nullable=False)
+    password_hash = Column(String(255))
     created = Column(DateTime, default=datetime.datetime.now)
     email_confirmed = Column(Boolean, nullable=False, default=False)
     is_super_user = Column(Boolean, nullable=False, default=False)
-    twitter = Column(String, nullable=True)
+    twitter = Column(String(16), nullable=True)
--- mlbpool/data/dbsession.py
+++ mlbpool/data/dbsession.py
@@ -1,5 +1,7 @@
 import sqlalchemy
 import sqlalchemy.orm
+import pymysql
+import mlbpool.data.config as config
 from mlbpool.data.modelbase import SqlAlchemyBase
 # noinspection PyUnresolvedReferences
 # import mlbpool.data.account
@@ -32,14 +34,14 @@ class DbSessionFactory:
 
     # Start a database session at app startup
     @staticmethod
-    def global_init(db_file):
-        if DbSessionFactory.factory:
-            return
+    def global_init():
+ #       if DbSessionFactory.factory:
+ #           return
 
-        if not db_file or not db_file.strip():
-            raise Exception("You must specify a data file.")
+ #       if not db_file or not db_file.strip():
+ #           raise Exception("You must specify a data file.")
 
-        conn_str = 'sqlite:///' + db_file
+        conn_str = 'mysql+pymysql://' + config.db_user + ':' + config.db_pw + '@localhost/mlbpooldb'
         print("Connecting to db with conn string: {}".format(conn_str))
 
         engine = sqlalchemy.create_engine(conn_str, echo=False)
