68eb3d207db7aa64dfe5e718188741c318efa357
Paul Cutler <paul.r.cutler@gmail.com>
2018-01-25 07:09:50 -0600

    Update docstrings in admin check query

--- mlbpool/services/admin_service.py
+++ mlbpool/services/admin_service.py
@@ -2,11 +2,10 @@ from mlbpool.data.dbsession import DbSessionFactory
 from mlbpool.data.account import Account
 
 
-'''Create a check that only the user who is True in the AccountInfo database with is_superuser can access
-the admin pages.  Make sure mlbpool/data/secret.py has an email assigned that matches the user during registration'''
-
-
 def admin_check():
+    """Create a check that only the user who is True in the AccountInfo database with is_superuser can access
+    the admin pages.  Make sure mlbpool/data/config.py has an email assigned that matches the user during
+    registration"""
     session = DbSessionFactory.create_session()
     su__query = session.query(Account.id).filter(Account.is_super_user == 1) \
         .filter(Account.id == self.logged_in_user_id).first()
