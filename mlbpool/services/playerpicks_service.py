4b425a83ecdcd685034a45cd6d177d80664d9581
Paul Cutler <paul.r.cutler@gmail.com>
2018-02-04 19:50:38 -0600

    Update the submit-picks template
    Update the picks controller to remove the datetime method if picks
    can be submitted.  This will be updated later when pick submissions
    are actually working.  Update the submit-pick template with all of
    the correct lists to iterate over in the Chameleon template.
    
    Note: this is still broken with a sqlalchemy timeout somehwere.

--- mlbpool/services/playerpicks_service.py
+++ mlbpool/services/playerpicks_service.py
@@ -10,14 +10,16 @@ import datetime
 class PlayerPicksService:
     """This class displays all of the drop down menus for the different picks pool players can make in
     submit_picks.pt"""
+
     @staticmethod
     def get_division_team_list(league_id, division_id):
         """Get a list of the five teams in a specific division in one of the two leagues (AL or NL)"""
         session = DbSessionFactory.create_session()
 
-        division_team_list = session.query(TeamInfo).filter(TeamInfo.league_id == league_id)\
+        division_team_list = session.query(TeamInfo).filter(TeamInfo.league_id == league_id) \
             .filter(TeamInfo.division_id == division_id).order_by(TeamInfo.name).all()
 
+        print(division_team_list)
         return division_team_list
 
     @staticmethod
@@ -26,7 +28,7 @@ class PlayerPicksService:
 
         # TODO Player list will be huge - need to add the team to the query to display on the picks page
 
-        player_list = session.query(ActiveMLBPlayers.player_id, ActiveMLBPlayers.firstname, ActiveMLBPlayers.lastname).\
+        player_list = session.query(ActiveMLBPlayers.player_id, ActiveMLBPlayers.firstname, ActiveMLBPlayers.lastname). \
             join(TeamInfo, ActiveMLBPlayers.team_id == TeamInfo.team_id) \
             .filter(TeamInfo.league_id == league_id) \
             .filter(ActiveMLBPlayers.position != position) \
@@ -39,7 +41,7 @@ class PlayerPicksService:
     def get_pitcher_list(league_id, position):
         session = DbSessionFactory.create_session()
 
-        player_list = session.query(ActiveMLBPlayers.player_id, ActiveMLBPlayers.firstname, ActiveMLBPlayers.lastname).\
+        player_list = session.query(ActiveMLBPlayers.player_id, ActiveMLBPlayers.firstname, ActiveMLBPlayers.lastname). \
             join(TeamInfo, ActiveMLBPlayers.team_id == TeamInfo.team_id) \
             .filter(TeamInfo.league_id == league_id) \
             .filter(ActiveMLBPlayers.position == position) \
@@ -90,64 +92,60 @@ class PlayerPicksService:
                          afc_pf_pick: int, nfc_pf_pick: int,
                          specialteams_td_pick: int,
                          user_id: str):
-
         session = DbSessionFactory.create_session()
         season_row = session.query(SeasonInfo.current_season).filter(SeasonInfo.id == 1).first()
         season = season_row.current_season
 
         dt = datetime.datetime.now()
 
-#        player_picks = PlayerPicks(date_submitted=dt, user_id=user_id, afc_east_first=afc_east_winner_pick,
-#                                   afc_east_second=afc_east_second, afc_east_last=afc_east_last,
-#                                   afc_north_first=afc_north_winner_pick, afc_north_second=afc_north_second,
-#                                   afc_north_last=afc_north_last,
-#                                   afc_south_first=afc_south_winner_pick, afc_south_second=afc_south_second,
-#                                   afc_south_last=afc_south_last,
-#                                   afc_west_first=afc_west_winner_pick, afc_west_second=afc_west_second,
-#                                   afc_west_last=afc_west_last,
-#                                   nfc_east_first=nfc_east_winner_pick,
-#                                   nfc_east_second=nfc_east_second, nfc_east_last=nfc_east_last,
-#                                   nfc_north_first=nfc_north_winner_pick, nfc_north_second=nfc_north_second,
-#                                   nfc_north_last=nfc_north_last,
-#                                   nfc_south_first=nfc_south_winner_pick, nfc_south_second=nfc_south_second,
-#                                   nfc_south_last=nfc_south_last,
-#                                   nfc_west_first=nfc_west_winner_pick,
-#                                   nfc_west_second=nfc_west_second,
-#                                   nfc_west_last=nfc_west_last,
-#                                   afc_passing_pick=afc_qb_pick,
-#                                   nfc_passing_pick=nfc_qb_pick,
-#                                   afc_rushing_pick=afc_rb_pick,
-#                                   nfc_rushing_pick=nfc_rb_pick,
-#                                   afc_receiving_pick=afc_rec_pick,
-#                                   nfc_receiving_pick=nfc_rec_pick,
-#                                   afc_sacks_pick=afc_sacks_pick,
-#                                   nfc_sacks_pick=nfc_sacks_pick,
-#                                   afc_int_pick=afc_int_pick,
-#                                   nfc_int_pick=nfc_int_pick,
-#                                   afc_wildcard1=afc_wildcard1_pick, afc_wildcard2=afc_wildcard2_pick,
-#                                   nfc_wildcard2=nfc_wildcard2_pick, nfc_wildcard1=nfc_wildcard1_pick,
-#                                   afc_pf=afc_pf_pick, nfc_pf=nfc_pf_pick,
-#                                   specialteams_td=specialteams_td_pick,
+        #        player_picks = PlayerPicks(date_submitted=dt, user_id=user_id, afc_east_first=afc_east_winner_pick,
+        #                                   afc_east_second=afc_east_second, afc_east_last=afc_east_last,
+        #                                   afc_north_first=afc_north_winner_pick, afc_north_second=afc_north_second,
+        #                                   afc_north_last=afc_north_last,
+        #                                   afc_south_first=afc_south_winner_pick, afc_south_second=afc_south_second,
+        #                                   afc_south_last=afc_south_last,
+        #                                   afc_west_first=afc_west_winner_pick, afc_west_second=afc_west_second,
+        #                                   afc_west_last=afc_west_last,
+        #                                   nfc_east_first=nfc_east_winner_pick,
+        #                                   nfc_east_second=nfc_east_second, nfc_east_last=nfc_east_last,
+        #                                   nfc_north_first=nfc_north_winner_pick, nfc_north_second=nfc_north_second,
+        #                                   nfc_north_last=nfc_north_last,
+        #                                   nfc_south_first=nfc_south_winner_pick, nfc_south_second=nfc_south_second,
+        #                                   nfc_south_last=nfc_south_last,
+        #                                   nfc_west_first=nfc_west_winner_pick,
+        #                                   nfc_west_second=nfc_west_second,
+        #                                   nfc_west_last=nfc_west_last,
+        #                                   afc_passing_pick=afc_qb_pick,
+        #                                   nfc_passing_pick=nfc_qb_pick,
+        #                                   afc_rushing_pick=afc_rb_pick,
+        #                                   nfc_rushing_pick=nfc_rb_pick,
+        #                                   afc_receiving_pick=afc_rec_pick,
+        #                                   nfc_receiving_pick=nfc_rec_pick,
+        #                                   afc_sacks_pick=afc_sacks_pick,
+        #                                   nfc_sacks_pick=nfc_sacks_pick,
+        #                                   afc_int_pick=afc_int_pick,
+        #                                   nfc_int_pick=nfc_int_pick,
+        #                                   afc_wildcard1=afc_wildcard1_pick, afc_wildcard2=afc_wildcard2_pick,
+        #                                   nfc_wildcard2=nfc_wildcard2_pick, nfc_wildcard1=nfc_wildcard1_pick,
+        #                                   afc_pf=afc_pf_pick, nfc_pf=nfc_pf_pick,
+        #                                   specialteams_td=specialteams_td_pick,
 
- #                                  season=season)
+        #                                  season=season)
 
+        #        session.add(player_picks)
 
+        #        session.commit()
 
-
-#        session.add(player_picks)
-
-#        session.commit()
-
-        #new stuff here --------------------
+        # new stuff here --------------------
         # Add AFC team picks
-        afc_east_winner_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, league_id=0, division_id=1, 
+        afc_east_winner_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, league_id=0, division_id=1,
                                          rank=1, team_id=afc_east_winner_pick, pick_type=1)
         session.add(afc_east_winner_db)
-        afc_east_second_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, league_id=0, division_id=1, 
+        afc_east_second_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, league_id=0, division_id=1,
                                          rank=2, team_id=afc_east_second, pick_type=1)
         session.add(afc_east_second_db)
-        
-        afc_east_last_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, league_id=0, division_id=1, 
+
+        afc_east_last_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, league_id=0, division_id=1,
                                        rank=4, team_id=afc_east_last, pick_type=1)
 
         session.add(afc_east_last_db)
@@ -258,9 +256,9 @@ class PlayerPicksService:
         afc_int_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, player_id=afc_int_pick,
                                  pick_type=8, league_id=0)
         session.add(afc_int_db)
-        
+
         # Add NFC Player Picks
-        
+
         nfc_passing_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, player_id=nfc_qb_pick,
                                      pick_type=4, league_id=1)
         session.add(nfc_passing_db)
@@ -289,7 +287,7 @@ class PlayerPicksService:
         afc_wildcard2_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, team_id=afc_wildcard2_pick,
                                        pick_type=9, league_id=0)
         session.add(afc_wildcard2_db)
-        
+
         nfc_wildcard1_db = PlayerPicks(user_id=user_id, season=season, date_submitted=dt, team_id=nfc_wildcard1_pick,
                                        pick_type=9, league_id=1)
         session.add(nfc_wildcard1_db)
@@ -318,16 +316,16 @@ class DisplayPlayerPicks:
 
     @staticmethod
     def display_picks(user_id):
-
         session = DbSessionFactory.create_session()
         season_row = session.query(SeasonInfo.current_season).filter(SeasonInfo.id == 1).first()
         season = season_row.current_season
 
-        user_query = session.query(PlayerPicks, TeamInfo.name).join(TeamInfo, PlayerPicks.afc_east_first == TeamInfo.team_id)\
+        user_query = session.query(PlayerPicks, TeamInfo.name).join(TeamInfo,
+                                                                    PlayerPicks.afc_east_first == TeamInfo.team_id) \
             .filter(PlayerPicks.user_id == user_id) \
             .filter(PlayerPicks.season == season).all()
 
-#        print(user_query)
-#        print(type(user_query[0]))
+        #        print(user_query)
+        #        print(type(user_query[0]))
 
         return user_query
