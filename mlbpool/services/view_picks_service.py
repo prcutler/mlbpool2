64c6b1385c5fd971b6655ea06c09dfc784e3afdc
Paul Cutler <paul.r.cutler@gmail.com>
2018-02-16 06:51:54 -0600

    Add two experimental methods to view pick service
    Add two methods to view picks to test the change picks template

--- mlbpool/services/view_picks_service.py
+++ mlbpool/services/view_picks_service.py
@@ -39,7 +39,45 @@ class ViewPicksService:
             .outerjoin(DivisionInfo, and_(PlayerPicks.division_id == DivisionInfo.division_id))\
             .outerjoin(ActiveMLBPlayers, and_(PlayerPicks.player_id == ActiveMLBPlayers.player_id,
                                               PlayerPicks.season == ActiveMLBPlayers.season)).\
-            filter(PlayerPicks.user_id == user_id,
-                   PlayerPicks.season == season)
+            filter(PlayerPicks.user_id == user_id, PlayerPicks.season == season)
 
         return picks_query
+
+    @staticmethod
+    def al_division_winner_picks(user_id, season):
+        """Return only the division winner picks in the American League (pick type 1, league_id 0, rank 1)"""
+
+        session = DbSessionFactory.create_session()
+
+        division_winners = session.query(PlayerPicks.pick_type, LeagueInfo.league_id, DivisionInfo.division_id,
+                                         TeamInfo.name, PlayerPicks.rank,
+                                         PlayerPicks.multiplier, PlayerPicks.changed) \
+            .outerjoin(LeagueInfo) \
+            .outerjoin(TeamInfo) \
+            .outerjoin(DivisionInfo, and_(PlayerPicks.division_id == DivisionInfo.division_id)) \
+            .outerjoin(ActiveMLBPlayers, and_(PlayerPicks.player_id == ActiveMLBPlayers.player_id,
+                                              PlayerPicks.season == ActiveMLBPlayers.season)). \
+            filter(PlayerPicks.user_id == user_id, PlayerPicks.season == season,
+                   PlayerPicks.league_id == 0, PlayerPicks.rank == 1, PlayerPicks.pick_type == 1)
+
+        return division_winners
+
+    @staticmethod
+    def al_east_picks(user_id, season):
+        """Return only the AL East division picks in the American League (pick type 1, league_id 0, division 1)"""
+
+        session = DbSessionFactory.create_session()
+
+        al_east_picks = session.query(PlayerPicks.pick_type, LeagueInfo.league_id, DivisionInfo.division_id,
+                                      TeamInfo.name, PlayerPicks.rank,
+                                      PlayerPicks.multiplier, PlayerPicks.changed) \
+            .outerjoin(LeagueInfo) \
+            .outerjoin(TeamInfo) \
+            .outerjoin(DivisionInfo, and_(PlayerPicks.division_id == DivisionInfo.division_id)) \
+            .outerjoin(ActiveMLBPlayers, and_(PlayerPicks.player_id == ActiveMLBPlayers.player_id,
+                                              PlayerPicks.season == ActiveMLBPlayers.season)). \
+            filter(PlayerPicks.user_id == user_id, PlayerPicks.season == season,
+                   PlayerPicks.league_id == 0, DivisionInfo.division_id == 1, PlayerPicks.pick_type == 1)
+
+        return al_east_picks
+
