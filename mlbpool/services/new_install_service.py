5fd01eeafdfcb69dd365e724068dbb73462d22cc
Paul Cutler <paul.r.cutler@gmail.com>
2018-02-18 08:57:07 -0600

    Update new_install_service to use config (not secret)
    Use config like the rest of the app (not secret from NFLPool)

--- mlbpool/services/new_install_service.py
+++ mlbpool/services/new_install_service.py
@@ -1,6 +1,6 @@
 from mlbpool.data.teaminfo import TeamInfo
 import requests
-import mlbpool.data.config as secret
+import mlbpool.data.config as config
 from requests.auth import HTTPBasicAuth
 from mlbpool.data.dbsession import DbSessionFactory
 from mlbpool.data.divisioninfo import DivisionInfo
@@ -28,7 +28,7 @@ class NewInstallService:
 
         response = requests.get(
             'https://api.mysportsfeeds.com/v1.2/pull/mlb/2018-regular/conference_team_standings.json',
-            auth=HTTPBasicAuth(secret.msf_username, secret.msf_pw))
+            auth=HTTPBasicAuth(config.msf_username, config.msf_pw))
 
         data = response.json()
 
