6e0b9143a97e4874a48f082f144b940ca2d43e87
Paul Cutler <paul.r.cutler@gmail.com>
2018-02-18 08:52:24 -0600

    Change default of changed in player_picks to 0 (from 1)
    Default pick should equal 0, not 1.  When a player changes their
    pick at the All-Star break, it will be changed to 1.

--- mlbpool/data/player_picks.py
+++ mlbpool/data/player_picks.py
@@ -16,5 +16,5 @@ class PlayerPicks(SqlAlchemyBase):
     player_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('ActiveMLBPlayers.player_id'))
     pick_type = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('PickTypes.pick_type_id'))
     twins_wins = sqlalchemy.Column(sqlalchemy.Integer)
-    changed = sqlalchemy.Column(sqlalchemy.Float, default=1)
+    changed = sqlalchemy.Column(sqlalchemy.Float, default=0)
     original_pick = sqlalchemy.Column(sqlalchemy.Integer)
