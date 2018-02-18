cc2044379ab4552cc70c720d05280a01cca0a253
Paul Cutler <paul.r.cutler@gmail.com>
2018-02-18 14:37:13 -0600

    Replace conference with league for MLBPool (Not NLFPool)

--- mlbpool/viewmodels/newinstallviewmodel.py
+++ mlbpool/viewmodels/newinstallviewmodel.py
@@ -7,7 +7,7 @@ class NewInstallViewModel(ViewModelBase):
         self.name = None
         self.city = None
         self.team_abbr = None
-        self.conference = None
+        self.league = None
         self.division = None
         self.division_abbr = None
         self.division_id = None
@@ -17,7 +17,7 @@ class NewInstallViewModel(ViewModelBase):
         self.name = data_dict.get('name')
         self.city = data_dict.get('city')
         self.team_abbr = data_dict.get('team_abbr')
-        self.conference = data_dict.get('conference')
+        self.league = data_dict.get('league')
         self.division = data_dict.get('division')
         self.division_abbr = data_dict.get('division_abbr')
         self.division_id = data_dict.get('division_id')
