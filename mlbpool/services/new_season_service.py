0f1f398cfa5e6fb00126a6fa1fe2018b01491906
Paul Cutler <paul.r.cutler@gmail.com>
2018-02-18 14:41:04 -0600

    Refactor new season service
    Refactor newseason_service to use the MSF API to get the date of
    the first game (rather than user input).  Also get the time of the
    game and the teams playing.  This will be used to display to the user
    how much time is remaining for them to enter their picks.

--- mlbpool/services/new_season_service.py
+++ mlbpool/services/new_season_service.py
@@ -1,5 +1,8 @@
 from mlbpool.data.seasoninfo import SeasonInfo
 from mlbpool.data.dbsession import DbSessionFactory
+import requests
+from requests.auth import HTTPBasicAuth
+import mlbpool.data.config as config
 
 
 class NewSeasonService:
@@ -7,29 +10,66 @@ class NewSeasonService:
     def get_install():
         return []
 
-    '''After first time installation or before a new season starts, this will update the season year
-    in the database.  This is used to for the MySportsFeeds API to get the correct year of stats needed.'''
     # TODO Add logging
     @classmethod
-    def create_season(cls, season, season_start_date, all_star_game_date):
+    def create_season(cls, season, all_star_game_date):
+        """After first time installation or before a new season starts, this will update the season year
+            in the database.  This is used to for the MySportsFeeds API to get the correct year of stats needed."""
         session = DbSessionFactory.create_session()
         season_row = session.query(SeasonInfo)
 
+        new_season = SeasonInfo()
+        new_season.current_season = season
+        new_season.all_star_game_date = all_star_game_date
+
         if season_row.count() == 0:
             print("New install, adding a season")
-            new_season = SeasonInfo()
-            new_season.current_season = season
-            new_season.season_start_date = season_start_date
-            new_season.all_star_game_date = all_star_game_date
+
+            response = requests.get('https://api.mysportsfeeds.com/v1.2/pull/mlb/' + str(season) +
+                                    '-regular/full_game_schedule.json',
+                                    auth=HTTPBasicAuth(config.msf_username, config.msf_pw))
+
+            gameday_json = response.json()
+            gameday_data = gameday_json["fullgameschedule"]["gameentry"][0]
+
+            first_game_date = gameday_data["date"]
+            first_game_time = gameday_data["time"]
+            away_team = gameday_data["awayTeam"]["ID"]
+            home_team = gameday_data["homeTeam"]["ID"]
+
+            new_season = SeasonInfo(season_start_date=first_game_date, season_start_time=first_game_time,
+                                    home_team=home_team, away_team=away_team, current_season=season,
+                                    all_star_game_date=all_star_game_date)
+
+            print(new_season)
+
             session.add(new_season)
             session.commit()
+            session.close()
         else:
             print("Existing season found, updating to new year")
+
+            response = requests.get('https://api.mysportsfeeds.com/v1.2/pull/mlb/' + str(season) +
+                                    '-regular/full_game_schedule.json',
+                                    auth=HTTPBasicAuth(config.msf_username, config.msf_pw))
+
+            gameday_json = response.json()
+            gameday_data = gameday_json["fullgameschedule"]["gameentry"][0]
+
+            first_game_date = gameday_data["date"]
+            first_game_time = gameday_data["time"]
+            away_team = gameday_data["awayTeam"]["ID"]
+            home_team = gameday_data["homeTeam"]["ID"]
+
+            season_start_date = first_game_date
 
             update_row = session.query(SeasonInfo).filter(SeasonInfo.id == '1').first()
             update_row.current_season = season
             update_row.season_start_date = season_start_date
             update_row.all_star_game_date = all_star_game_date
+            update_row.first_game_time = first_game_time
+            update_row.away_team = away_team
+            update_row.home_team = home_team
 
             session.commit()
-
+            session.close()
