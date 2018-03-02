6b772a526a42997d1be5b4df6a6070c52ddd1b19
Paul Cutler <paul.r.cutler@gmail.com>
2018-03-02 07:51:19 -0600

    Start to re-write the StandingsService
    Replace all NFLPool references (conference instead of league) and
    change wildcard rankings (4 & 5, not 5 & 6 from NFLPool)
    
    Add lots of code comments to help Kelly out for the upcoming rewrite

--- mlbpool/services/standings_service.py
+++ mlbpool/services/standings_service.py
@@ -43,14 +43,14 @@ class StandingsService:
             season = get_seasons()
 
         sqlstr = "SELECT coalesce(w.points_earned,0) as points, a.first_name, a.last_name, w.pick_id, p.pick_type, p.rank, p.multiplier, t.name, "
-        sqlstr += "c.conference, d.division, ap.firstname, ap.lastname "
+        sqlstr += "c.league, d.division, ap.firstname, ap.lastname "
         sqlstr += "FROM PlayerPicks p, Account a "
         sqlstr += "LEFT JOIN WeeklyPlayerResults w on p.pick_id = w.pick_id "
         sqlstr += "AND w.week = (SELECT MAX(week) from WeeklyPlayerResults  WHERE season=" + str(season) + ") "
         sqlstr += "LEFT JOIN  DivisionInfo d on p.division_id=d.division_id "
-        sqlstr += "LEFT JOIN ConferenceInfo c ON p.conf_id= c.conf_id "
+        sqlstr += "LEFT JOIN LeagueInfo c ON p.league_id= c.league_id "
         sqlstr += "LEFT JOIN TeamInfo t ON p.team_id = t.team_id "
-        sqlstr += "LEFT JOIN ActiveNFLPlayers ap ON p.player_id = ap.player_id AND p.season = ap.season "
+        sqlstr += "LEFT JOIN ActiveMLBPlayers ap ON p.player_id = ap.player_id AND p.season = ap.season "
         sqlstr += "WHERE "
         sqlstr += "p.user_id = a.id "
         sqlstr += "AND p.season = " + str(season) + " "
@@ -89,51 +89,60 @@ class StandingsService:
 
     @staticmethod
     def update_player_pick_points():
+
+        # TODO Kelly - Is this where we add / calculate the ERA and BA qualifiers?
+        """ Batting Average qualifier - A player must have 3.1 plate appearances (PA) per team game (
+        for a total of 502 over the current 162-game season) to qualify for the batting title.
+        Plate Appearances is a column in WeeklyMLBPlayers.  team_games_played is in WeeklyTeamStats.
+
+        A pitcher to pitch one inning per game played by their team. In most years, that is 162 innings.
+        team_games_played is in WeeklyTeamStats."""
+
         season = get_seasons()
         week = get_week()
 
         session = DbSessionFactory.create_session()
 
         # starting values
-        conf = 0
+        league = 0
         i = 4
 
-        while (conf < 2):
+        while league < 2:
             # start with pick type 4 and continue through 8
 
             if i == 4:
-                cattype = "passyds"
+                cattype = "homeruns"
             elif i == 5:
-                cattype ="rushyds"
+                cattype ="batting_average"
             elif i == 6:
-                cattype ="recyds"
+                cattype ="RBI"
             elif i == 7:
-                cattype ="sacks"
+                cattype ="pitcher_wins"
             elif i == 8:
-                cattype = "interceptions"
+                cattype = "ERA"
 
 
             sqlstr = "INSERT INTO WeeklyPlayerResults (pick_id, season, week, points_earned) "
             sqlstr += "SELECT t1.pick_id as pick_id, t1.season as season, t1.week as week, pts.points*t1.multiplier as points_earned "
             sqlstr += "FROM "
             sqlstr += "(SELECT p.pick_id, p.user_id, p.multiplier, p.player_id, "
-            sqlstr += "(SELECT count(*) from WeeklyNFLPlayerStats as w2, ActiveNFLPlayers as ap, "
+            sqlstr += "(SELECT count(*) from WeeklyMLBPlayerStats as w2, ActiveMLBPlayers as ap, "
             sqlstr += "TeamInfo as t "
             sqlstr += "WHERE "
             sqlstr += "w2.week = " + str(week) + " "
             sqlstr += "AND w2.season = " + str(season) + " "
             sqlstr += "AND w2.player_id = ap.player_id "
             sqlstr += "AND ap.team_id = t.team_id "
-            sqlstr += "AND t.conference_id = " + str(conf) + " "
+            sqlstr += "AND t.league_id = " + str(league) + " "
             sqlstr += "AND W2." + cattype + ">w." + cattype + ")+1 as rank, "
             sqlstr += "w.week, "
             sqlstr += "w.season "
-            sqlstr += "FROM WeeklyNFLPlayerStats w, PlayerPicks p "
+            sqlstr += "FROM WeeklyMLBPlayerStats w, PlayerPicks p "
             sqlstr += "WHERE w.player_id = p.player_id "
             sqlstr += "AND  w.season = " + str(season) + " "
             sqlstr += "AND w.week = " + str(week) + " "
             sqlstr += "AND p.pick_type = " + str(i) + " "
-            sqlstr += "AND p.conf_id = " + str(conf) + " "
+            sqlstr += "AND p.league_id = " + str(league) + " "
             sqlstr += "AND w." + cattype + " not null "
             sqlstr += "ORDER BY rank) as t1, PickTypePoints pts "
             sqlstr += "WHERE "
@@ -145,8 +154,8 @@ class StandingsService:
 
             # increment counters
             if i == 8:
-                conf += 1
-                i=0
+                league += 1
+                i = 0
 
             i += 1
 
@@ -169,7 +178,7 @@ class StandingsService:
         sqlstr += "WHERE pp.pick_type = 1 "
         sqlstr += "AND w.season = " + str(season) + " "
         sqlstr += "AND w.week = " + str(week) + " "
-        sqlstr += "AND pp.conf_id = t.conference_id "
+        sqlstr += "AND pp.league_id = t.league_id "
         sqlstr += "AND pp.division_id = t.division_id "
         sqlstr += "AND p.place = w.division_rank "
         sqlstr += "ORDER BY pp.user_id"
@@ -177,9 +186,10 @@ class StandingsService:
         session.execute(sqlstr)
         session.commit()
 
-        # type 3 points:
-        conf = 0
-        while conf<2:
+        # type 3 team wins points:
+        # TODO Need a similar function for type 2 - team losses - Kelly
+        league = 0
+        while league < 2:
             sqlstr = "INSERT INTO WeeklyPlayerResults(pick_id, season, week, points_earned) "
             sqlstr += "SELECT t1.pick_id as pick_id, t1.season as season, t1.week as week, pts.points * t1.multiplier as points_earned "
             sqlstr += "FROM (SELECT p.pick_id, p.user_id, p.multiplier, p.team_id, "
@@ -187,7 +197,7 @@ class StandingsService:
             sqlstr += "WHERE w2.team_id = t.team_id "
             sqlstr += "AND w2.week = " + str(week) + " "
             sqlstr += "AND w2.season = " + str(season) + " "
-            sqlstr += "AND t.conference_id = " + str(conf) + " "
+            sqlstr += "AND t.league_id = " + str(league) + " "
             sqlstr += "AND w2.points_for > w.points_for)+1 as rank, "
             sqlstr += "w.week, w.season "
             sqlstr += "FROM WeeklyTeamStats w, PlayerPicks p "
@@ -195,7 +205,7 @@ class StandingsService:
             sqlstr += "AND w.season = " + str(season) + " "
             sqlstr += "AND w.week = " + str(week) + " "
             sqlstr += "AND p.pick_type = 3 "
-            sqlstr += "AND p.conf_id = " + str(conf) + " "
+            sqlstr += "AND p.league_id = " + str(league) + " "
             sqlstr += "AND w.points_for not null "
             sqlstr += "ORDER BY rank) as t1, PickTypePoints pts "
             sqlstr += "WHERE "
@@ -204,14 +214,14 @@ class StandingsService:
 
             session.execute(sqlstr)
             session.commit()
-            conf +=1
+            league += 1
 
-        # type 9 points - wildcard
+        # type 9 points - wildcard - (rank 4,5 - NFLPool was 5,6)
         sqlstr = "INSERT INTO WeeklyPlayerResults (pick_id, season, week, points_earned) "
         sqlstr += "SELECT p.pick_id, w.season, w.week, pts.points*p.multiplier as points_earned from PlayerPicks p, WeeklyTeamStats w, PickTypePoints pts "
         sqlstr += "WHERE p.pick_type = 9 "
         sqlstr += "AND p.pick_type = pts.pick_type_id "
-        sqlstr += "AND w.conference_rank in (5,6) "
+        sqlstr += "AND w.league_rank in (4,5) "
         sqlstr += "AND w.team_id = p.team_id "
         sqlstr += "AND w.season = " + str(season) + " "
         sqlstr += "AND w.week = " + str(week)
