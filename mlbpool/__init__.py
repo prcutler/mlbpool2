04383caaa051f83d1b1f3cfa64edd3ad2bb3097f
Paul Cutler <paul.r.cutler@gmail.com>
2018-03-05 16:51:12 -0600

    Turn on logging
    We're getting closer to launching and now need logging!  Note:
    Need to update the log_package_versions requires

--- mlbpool/__init__.py
+++ mlbpool/__init__.py
@@ -9,7 +9,7 @@ import os
 import mlbpool
 from mlbpool.services.email_service import EmailService
 from mlbpool.services.email_service import EmailTemplateParser
-# from mlbpool.services.log_service import LogService
+from mlbpool.services.log_service import LogService
 import pkg_resources
 import datetime
 import sys
@@ -22,7 +22,7 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
 
-#    init_logging(config)  # runs first
+    init_logging(config)  # runs first
     init_mode(config)
     init_includes(config)
     init_routing(config)
@@ -80,8 +80,8 @@ def init_mode(config):
     global dev_mode
     settings = config.get_settings()
     dev_mode = settings.get('mode') == 'dev'
-#    log = LogService.get_startup_log()
-#    log.notice('Running in {} mode.'.format('dev' if dev_mode else 'prod'))
+    log = LogService.get_startup_log()
+    log.notice('Running in {} mode.'.format('dev' if dev_mode else 'prod'))
 
 
 def init_routing(config):
@@ -112,53 +112,53 @@ def init_includes(config):
 #    (config.include('rollbar.contrib.pyramid'))
 
 
-# def log_package_versions():
-#    startup_log = LogService.get_startup_log()
+def log_package_versions():
+    startup_log = LogService.get_startup_log()
 
     # update from setup.py when changed!
     # This list is the closure of all dependencies,
     # taken from: pip list --format json
-#    requires = [{"name": "arrow", "version": "0.10.0"}, {"name": "binaryornot", "version": "0.4.3"},
-#                {"name": "certifi", "version": "2017.4.17"}, {"name": "Chameleon", "version": "3.1"},
-#                {"name": "chardet", "version": "3.0.4"}, {"name": "click", "version": "6.7"},
-#                {"name": "cookiecutter", "version": "1.5.1"}, {"name": "future", "version": "0.16.0"},
-#                {"name": "html2text", "version": "2016.9.19"}, {"name": "hupper", "version": "1.0"},
-#                {"name": "idna", "version": "2.5"}, {"name": "Jinja2", "version": "2.9.6"},
-#                {"name": "jinja2-time", "version": "0.2.0"}, {"name": "Logbook", "version": "1.1.0"},
-#                {"name": "mailer", "version": "0.8.1"}, {"name": "Mako", "version": "1.0.6"},
-#                {"name": "MarkupSafe", "version": "1.0"}, {"name": "mlbpool", "version": "0.0"},
-#                {"name": "passlib", "version": "1.7.1"}, {"name": "PasteDeploy", "version": "1.5.2"},
-#                {"name": "pip", "version": "9.0.1"}, {"name": "plaster", "version": "0.5"},
-#                {"name": "plaster-pastedeploy", "version": "0.4.1"}, {"name": "poyo", "version": "0.4.1"},
-#                {"name": "Pygments", "version": "2.2.0"}, {"name": "pymongo", "version": "3.4.0"},
-#                {"name": "pyramid", "version": "1.9.1"}, {"name": "pyramid-chameleon", "version": "0.3"},
-#                {"name": "pyramid-debugtoolbar", "version": "4.2.1"}, {"name": "pyramid-handlers", "version": "0.5"},
-#                {"name": "pyramid-jinja2", "version": "2.7"}, {"name": "pyramid-mako", "version": "1.0.2"},
-#                {"name": "python-dateutil", "version": "2.6.0"}, {"name": "repoze.lru", "version": "0.6"},
-#                {"name": "requests", "version": "2.18.1"}, {"name": "rollbar", "version": "0.13.12"},
-#                {"name": "setuptools", "version": "36.2.0"}, {"name": "six", "version": "1.10.0"},
-#                {"name": "SQLAlchemy", "version": "1.1.12"}, {"name": "translationstring", "version": "1.3"},
-#                {"name": "urllib3", "version": "1.21.1"}, {"name": "venusian", "version": "1.1.0"},
-#                {"name": "waitress", "version": "1.0.2"}, {"name": "WebOb", "version": "1.7.3"},
-#                {"name": "whichcraft", "version": "0.4.1"}, {"name": "zope.deprecation", "version": "4.2.0"},
-#                {"name": "zope.interface", "version": "4.4.2"}]
+    requires = [{"name": "arrow", "version": "0.10.0"}, {"name": "binaryornot", "version": "0.4.3"},
+                {"name": "certifi", "version": "2017.4.17"}, {"name": "Chameleon", "version": "3.1"},
+                {"name": "chardet", "version": "3.0.4"}, {"name": "click", "version": "6.7"},
+                {"name": "cookiecutter", "version": "1.5.1"}, {"name": "future", "version": "0.16.0"},
+                {"name": "html2text", "version": "2016.9.19"}, {"name": "hupper", "version": "1.0"},
+                {"name": "idna", "version": "2.5"}, {"name": "Jinja2", "version": "2.9.6"},
+                {"name": "jinja2-time", "version": "0.2.0"}, {"name": "Logbook", "version": "1.1.0"},
+                {"name": "mailer", "version": "0.8.1"}, {"name": "Mako", "version": "1.0.6"},
+                {"name": "MarkupSafe", "version": "1.0"}, {"name": "mlbpool", "version": "0.0"},
+                {"name": "passlib", "version": "1.7.1"}, {"name": "PasteDeploy", "version": "1.5.2"},
+                {"name": "pip", "version": "9.0.1"}, {"name": "plaster", "version": "0.5"},
+                {"name": "plaster-pastedeploy", "version": "0.4.1"}, {"name": "poyo", "version": "0.4.1"},
+                {"name": "Pygments", "version": "2.2.0"}, {"name": "pymongo", "version": "3.4.0"},
+                {"name": "pyramid", "version": "1.9.1"}, {"name": "pyramid-chameleon", "version": "0.3"},
+                {"name": "pyramid-debugtoolbar", "version": "4.2.1"}, {"name": "pyramid-handlers", "version": "0.5"},
+                {"name": "pyramid-jinja2", "version": "2.7"}, {"name": "pyramid-mako", "version": "1.0.2"},
+                {"name": "python-dateutil", "version": "2.6.0"}, {"name": "repoze.lru", "version": "0.6"},
+                {"name": "requests", "version": "2.18.1"}, {"name": "rollbar", "version": "0.13.12"},
+                {"name": "setuptools", "version": "36.2.0"}, {"name": "six", "version": "1.10.0"},
+                {"name": "SQLAlchemy", "version": "1.1.12"}, {"name": "translationstring", "version": "1.3"},
+                {"name": "urllib3", "version": "1.21.1"}, {"name": "venusian", "version": "1.1.0"},
+                {"name": "waitress", "version": "1.0.2"}, {"name": "WebOb", "version": "1.7.3"},
+                {"name": "whichcraft", "version": "0.4.1"}, {"name": "zope.deprecation", "version": "4.2.0"},
+                {"name": "zope.interface", "version": "4.4.2"}]
 
-#    requires.sort(key=lambda d: d['name'].lower())
-#    t0 = datetime.datetime.now()
-#    startup_log.notice('---------- Python version info ------------------')
-#    startup_log.notice(sys.version.replace('\n', ' ').replace('  ', ' '))
-#    startup_log.notice('---------- package version info ------------------')
-#    for rec in requires:
-#        try:
-#            version = pkg_resources.get_distribution(rec['name']).version
-#            if version:
-#                startup_log.notice('{} v{}'.format(rec['name'], version))
-#            else:
-#                startup_log.notice("WHERE IS IT? {}.".format(rec['name']))
-#        except Exception as x:
-#            startup_log.notice('{} UNKNOWN VERSION ({})'.format(rec['name'], x))
+    requires.sort(key=lambda d: d['name'].lower())
+    t0 = datetime.datetime.now()
+    startup_log.notice('---------- Python version info ------------------')
+    startup_log.notice(sys.version.replace('\n', ' ').replace('  ', ' '))
+    startup_log.notice('---------- package version info ------------------')
+    for rec in requires:
+        try:
+            version = pkg_resources.get_distribution(rec['name']).version
+            if version:
+                startup_log.notice('{} v{}'.format(rec['name'], version))
+            else:
+                startup_log.notice("WHERE IS IT? {}.".format(rec['name']))
+        except Exception as x:
+            startup_log.notice('{} UNKNOWN VERSION ({})'.format(rec['name'], x))
 
-#    dt = datetime.datetime.now() - t0
+    dt = datetime.datetime.now() - t0
 
-#    startup_log.notice('Package info gathered in {} sec'.format(dt.total_seconds()))
-#    startup_log.notice('--------------------------------------------------')
+    startup_log.notice('Package info gathered in {} sec'.format(dt.total_seconds()))
+    startup_log.notice('--------------------------------------------------')
